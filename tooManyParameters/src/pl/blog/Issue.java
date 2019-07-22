31cb8a8cb52cc4255e4708f9819207cc7b5bdf4c
Klimek <kamilklimek97@gmail.com>
2019-07-22 07:51:05 +0100

    Too many parameters solution

--- tooManyParameters/src/pl/blog/Issue.java
+++ tooManyParameters/src/pl/blog/Issue.java
@@ -3,36 +3,124 @@ package pl.blog;
 import java.time.OffsetDateTime;
 
 class Issue {
-    private String summary;
-    private String description;
-    private String reporterEmail;
-    private String assigneeEmail;
-    private IssueType type;
-    private Priority priority;
-    private OffsetDateTime createdDate;
-    private IssueStatus status;
-    private Component component;
+    private final String summary;
+    private final String description;
+    private final String reporterEmail;
+    private final String assigneeEmail;
+    private final IssueType type;
+    private final Priority priority;
+    private final OffsetDateTime createdDate;
+    private final IssueStatus status;
+    private final Component component;
 
-    Issue(String summary, String reporterEmail, IssueType type, Priority priority, OffsetDateTime createdDate, IssueStatus status) {
+    private Issue(String summary, String description, String reporterEmail, String assigneeEmail, IssueType type, Priority priority, OffsetDateTime createdDate, IssueStatus status, Component component) {
         this.summary = summary;
+        this.description = description;
         this.reporterEmail = reporterEmail;
+        this.assigneeEmail = assigneeEmail;
         this.type = type;
         this.priority = priority;
         this.createdDate = createdDate;
         this.status = status;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setAssigneeEmail(String assigneeEmail) {
-        this.assigneeEmail = assigneeEmail;
-    }
-
-    public void setComponent(Component component) {
         this.component = component;
     }
+
+    private static class Builder implements NeedSummary, NeedCreatedDate, NeedPriority, NeedReporter, NeedType, CanBeBuild {
+        private String summary;
+        private String description;
+        private String reporterEmail;
+        private String assigneeEmail;
+        private IssueType type;
+        private Priority priority;
+        private OffsetDateTime createdDate;
+        private IssueStatus status;
+        private Component component;
+        
+        @Override
+        public NeedReporter summary(String summary) {
+            this.summary = summary;
+            return this;
+        }
+
+        @Override
+        public NeedPriority reporter(String reporterEmail) {
+            this.reporterEmail = reporterEmail;
+            return this;
+        }
+
+        @Override
+        public NeedCreatedDate createdDate(OffsetDateTime offsetDateTime) {
+            this.createdDate = offsetDateTime;
+            return this;
+        }
+
+        @Override
+        public NeedType status(IssueStatus status) {
+            this.status = status;
+            return this;
+        }
+
+        @Override
+        public CanBeBuild type(IssueType issueType) {
+            this.type = issueType;
+            return this;
+        }
+
+        @Override
+        public CanBeBuild description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        @Override
+        public CanBeBuild assignee(String assigneeEmail) {
+            this.assigneeEmail = assigneeEmail;
+            return this;
+        }
+
+        @Override
+        public CanBeBuild component(Component component) {
+            this.component = component;
+            return this;
+        }
+
+        @Override
+        public Issue build() {
+            return new Issue(summary, description, reporterEmail, assigneeEmail, type, priority, createdDate, status, component);
+        }
+    }
+
+    static NeedSummary builder() {
+        return new Builder();
+    }
+
+    interface NeedSummary {
+        NeedReporter summary(String summary);
+    }
+
+    interface NeedReporter {
+        NeedPriority reporter(String reporterEmail);
+    }
+
+    interface NeedPriority {
+        NeedCreatedDate createdDate(OffsetDateTime offsetDateTime);
+    }
+
+    interface NeedCreatedDate {
+        NeedType status(IssueStatus status);
+    }
+
+    interface NeedType {
+        CanBeBuild type(IssueType issueType);
+    }
+
+    interface CanBeBuild {
+        CanBeBuild description(String description);
+        CanBeBuild assignee(String assigneeEmail);
+        CanBeBuild component(Component component);
+        Issue build();
+    }
+
 
     public String getSummary() {
         return summary;
--- tooManyParameters/src/pl/blog/Main.java
+++ tooManyParameters/src/pl/blog/Main.java
@@ -6,10 +6,19 @@ public class Main {
     public static void main(String[] args) {
 
         // How to avoid this ugly following record? How it can be done better?
-        Issue issue = new Issue("Loading screen error", "client@mail.com", IssueType.BUG, Priority.HIGH, OffsetDateTime.now(), IssueStatus.WAITING);
+        /*Issue issue = new Issue("Loading screen error", "client@mail.com", IssueType.BUG, Priority.HIGH, OffsetDateTime.now(), IssueStatus.WAITING);
         issue.setComponent(Component.MOBILE_APP);
         issue.setDescription("I can't use mobile application");
-        issue.setAssigneeEmail("support@mail.com");
+        issue.setAssigneeEmail("support@mail.com");*/
+
+        Issue issue = Issue.builder()
+                .summary("Summary")
+                .reporter("Reporter@email.com")
+                .createdDate(OffsetDateTime.now())
+                .status(IssueStatus.IN_PROGRESS)
+                .type(IssueType.CLIENT_ISSUE)
+                .component(Component.DESKTOP_APP)
+                .build();
 
     }
 }
